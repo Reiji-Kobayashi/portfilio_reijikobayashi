55b4e5020ce67bc5fe2873a415c64497a9ae708e
Reiji Kobayashi <k2130045@edu.cc.uec.ac.jp>
2022-08-27 21:51:38 +0900

    router設定を含めた基本構成の実装

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Portfolio Reiji Kobayashi</h1>
+  <router-view></router-view>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
